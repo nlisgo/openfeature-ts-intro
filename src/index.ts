8c1e4881d3382b98aca085dee23e1a98be8dd58b
Nathan Lisgo <nathan@lisgo.co.uk>
2024-11-12 17:49:26 +0000

    Add Joi validation for config and update welcome message handling

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,21 @@ import { OpenFeature } from '@openfeature/server-sdk';
 import { EnvVarProvider } from '@openfeature/env-var-provider';
 import { FlagdProvider } from '@openfeature/flagd-provider';
 import { FirstSuccessfulStrategy, MultiProvider } from '@openfeature/multi-provider';
+import Joi from 'joi';
+
+type Config = {
+  showWelcomeMessage: boolean,
+  welcomeMessage?: string,
+};
+
+const defaultConfig: Config = {
+  showWelcomeMessage: false,
+};
+
+const ConfigSchema = Joi.object<Config, true>({
+  showWelcomeMessage: Joi.boolean().required(),
+  welcomeMessage: Joi.string().optional(),
+});
 
 const app = express();
 const port = 3000;
@@ -48,11 +63,27 @@ switch (process.env.OPENFEATURE_PROVIDER) {
 
 const client = OpenFeature.getClient();
 
+const getConfig = async () => Joi.attempt(
+  await client.getObjectValue('config', defaultConfig),
+  ConfigSchema,
+  {
+    allowUnknown: true,
+  }
+);
+
 app.get('/', async (_, res) => {
   const showWelcomeMessage = await client.getBooleanValue('show-welcome-message', false);
   const welcomeMessage = await client.getStringValue('welcome-message', 'Default welcome message');
-  const moreFun = await client.getStringValue('more-fun', '');
-  res.send(`Express + TypeScript${showWelcomeMessage ? ' + OpenFeature Server' : ''}: ${welcomeMessage}${!!moreFun ? ` (${moreFun})` : ''}`);
+  const moreFun = await client.getStringValue('more-fun', 'No more-fun!!');
+  const config = await getConfig().catch((error) => {
+    console.warn(error);
+    return defaultConfig;
+  });
+  console.log(config);
+  res.send(`
+    Express + TypeScript${showWelcomeMessage ? ' + OpenFeature Server' : ''}: ${welcomeMessage}${!!moreFun ? ` (${moreFun})` : ''}
+    ${JSON.stringify(config)}
+  `);
 });
 
 app.listen(port, () => {
